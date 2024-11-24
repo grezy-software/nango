5dc9e4bfc1da28d1813d9f2dc77901e55733ba65
Xenepix <Firenix.nex@gmail.com>
2024-11-24 15:20:30 +0100

    feat(runner): add run_cogs method to execute cogs for each (not filtered model)

--- backend/nango/utils/cogs_runner.py
+++ backend/nango/utils/cogs_runner.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections.abc import Callable
 from importlib import import_module
 from typing import TYPE_CHECKING
 
@@ -15,6 +16,11 @@ class CogsRunner:
     """Load and execute cogs.
 
     For the order execution, follow the nango.cogs.__all__ order.
+
+    Settings:
+    --------
+    Nango's settings can be defined for each cog at the model level.
+    A model can have a staticmethod called `nango` that returns a None (for no generation) or a dict (with settings for each cogs).
     """
 
     model_filter_keywords: list[str] = ("django", "django_celery_beat", "rest_framework")
@@ -25,11 +31,19 @@ class CogsRunner:
 
     def get_settings_from_model(self, model: models.Model) -> dict[str, any]:
         """Return settings define in a model."""
+        model_nango_method: Callable | None = getattr(model, "nango", None)
+        if not isinstance(model_nango_method, Callable):
+            if model_nango_method is None:
+                # No settings defined in the model
+                return {}
+            error_msg: str = f"{model}.nango must be a staticmethod, not a property."
+            raise TypeError(error_msg)
+        return model_nango_method()
 
     def _is_filtered_model(self, model: models.Model) -> bool:
         """Indicate if a model is filter or not.
 
-        If the model is filter, we remove it.
+        If the model is filter, we don't run it.
         """
         return any(keyword in str(model) for keyword in self.model_filter_keywords)
 
@@ -39,10 +53,16 @@ class CogsRunner:
 
         for model in apps.get_models():
             if self._is_filtered_model(model):
-                return
-
+                continue
             for cog in cogs_classes:
-                instanciated_cog = cog(model=model, settings=self.get_settings_from_model(model))  # noqa: F841
+                settings = self.get_settings_from_model(model)
+                if settings is None:
+                    # This model doesn't want any cogs.
+                    continue
+
+                # Run the cogs
+                instantiated_cog = cog(model=model, settings=settings)
+                instantiated_cog.run()
 
 
 if __name__ == "django.core.management.commands.shell":
